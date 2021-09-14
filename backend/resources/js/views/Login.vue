61c681734a1c861b7d1ccfecdba0c4a9261ac142
soregashi-27 <tomo.soul.15@gmail.com>
2021-09-15 07:48:35 +0900

    ログインコンポーネントの追加

--- backend/resources/js/views/Login.vue
+++ backend/resources/js/views/Login.vue
@@ -13,9 +13,21 @@
         <v-card-text>
           <v-form>
             <v-text-field tabindex="1" label="メールアドレス" prepend-icon="mdi-email" />
-            <v-text-field tabindex="2" label="パスワード" />
+            <v-text-field tabindex="2" label="パスワード" prepend-icon="mdi-lock" />
           </v-form>
         </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-tooltip button>
+            <template>
+              <v-btn text color="success">テストログイン</v-btn>
+            </template>
+            <span> <v-icon dark left>mdi-alert-circle</v-icon>一定期間が立つとアカウントは初期化されます。 </span>
+          </v-tooltip>
+
+          <v-btn color="info">ログイン</v-btn>
+        </v-card-actions>
       </v-card>
     </v-col>
   </v-row>
