471d957540e25d59e558bef186665da1a8c50a5b
soregashi-27 <tomo.soul.15@gmail.com>
2021-09-14 15:46:04 +0900

    ルーティングを追加（Vue Router）

---
+++ backend/resources/js/router.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Login from './views/Login.vue'
+import Register from './views/Register.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    component: Login
+  },
+  {
+    path: '/register',
+    component: Register
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
