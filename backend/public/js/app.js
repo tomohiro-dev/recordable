5c32c09cce77e3fdf6836e48804ec546345bf142
soregashi-27 <tomo.soul.15@gmail.com>
2021-09-15 08:42:29 +0900

    パスワードのマスキング追加

--- backend/public/js/app.js
+++ backend/public/js/app.js
@@ -1905,7 +1905,31 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({});
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
+  data: function data() {
+    return {
+      snackbar: false,
+      showPassword: false,
+      loginForm: {
+        email: '',
+        password: ''
+      }
+    };
+  }
+});
 
 /***/ }),
 
@@ -1938,13 +1962,15 @@ __webpack_require__.r(__webpack_exports__);
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm.js");
+/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm.js");
 /* harmony import */ var _router__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./router */ "./resources/js/router.js");
 /* harmony import */ var _App_vue__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./App.vue */ "./resources/js/App.vue");
-/* harmony import */ var vuetify__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! vuetify */ "./node_modules/vuetify/dist/vuetify.js");
-/* harmony import */ var vuetify__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(vuetify__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var vuetify__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! vuetify */ "./node_modules/vuetify/dist/vuetify.js");
+/* harmony import */ var vuetify__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(vuetify__WEBPACK_IMPORTED_MODULE_6__);
 /* harmony import */ var vuetify_dist_vuetify_min_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! vuetify/dist/vuetify.min.css */ "./node_modules/vuetify/dist/vuetify.min.css");
 /* harmony import */ var _mdi_font_css_materialdesignicons_css__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @mdi/font/css/materialdesignicons.css */ "./node_modules/@mdi/font/css/materialdesignicons.css");
+/* harmony import */ var vue_the_mask__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! vue-the-mask */ "./node_modules/vue-the-mask/dist/vue-the-mask.js");
+/* harmony import */ var vue_the_mask__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(vue_the_mask__WEBPACK_IMPORTED_MODULE_4__);
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -1958,26 +1984,18 @@ __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
 
 
-window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm.js");
-vue__WEBPACK_IMPORTED_MODULE_4__.default.use((vuetify__WEBPACK_IMPORTED_MODULE_5___default()));
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-new vue__WEBPACK_IMPORTED_MODULE_4__.default({
+window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm.js");
+vue__WEBPACK_IMPORTED_MODULE_5__.default.use((vuetify__WEBPACK_IMPORTED_MODULE_6___default()));
+vue__WEBPACK_IMPORTED_MODULE_5__.default.use((vue_the_mask__WEBPACK_IMPORTED_MODULE_4___default()));
+new vue__WEBPACK_IMPORTED_MODULE_5__.default({
   el: '#app',
   router: _router__WEBPACK_IMPORTED_MODULE_0__.default,
   components: {
     App: _App_vue__WEBPACK_IMPORTED_MODULE_1__.default
   },
   template: '<App />',
-  vuetify: new (vuetify__WEBPACK_IMPORTED_MODULE_5___default())()
+  vuetify: new (vuetify__WEBPACK_IMPORTED_MODULE_6___default())()
 });
 
 /***/ }),
@@ -38305,84 +38323,117 @@ var render = function() {
   return _c(
     "v-row",
     [
+      _c("v-spacer"),
+      _vm._v(" "),
+      _c("v-col", { staticClass: "mt-n11", attrs: { cols: "12", md: "5" } }),
+      _vm._v(" "),
+      _c("v-spacer"),
+      _vm._v(" "),
       _c(
-        "v-col",
-        { attrs: { cols: "12", md: "5" } },
+        "v-card",
         [
+          _c("v-card-title", [_c("h1", [_vm._v("ログイン画面だよ")])]),
+          _vm._v(" "),
+          _c("v-card-title", { staticClass: "pb-0" }, [
+            _c("h2", { staticClass: "title" }, [_vm._v("ログイン")])
+          ]),
+          _vm._v(" "),
           _c(
-            "v-card",
+            "v-card-text",
             [
-              _c("v-card-title", [_c("h1", [_vm._v("ログイン画面だよ")])]),
-              _vm._v(" "),
-              _c("v-card-title", [
-                _c("h2", { staticClass: "title" }, [_vm._v("ログイン")])
-              ]),
+              _c(
+                "v-form",
+                [
+                  _c("v-text-field", {
+                    attrs: {
+                      tabindex: "1",
+                      label: "メールアドレス",
+                      "prepend-icon": "mdi-email"
+                    },
+                    model: {
+                      value: _vm.loginForm.email,
+                      callback: function($$v) {
+                        _vm.$set(_vm.loginForm, "email", $$v)
+                      },
+                      expression: "loginForm.email"
+                    }
+                  }),
+                  _vm._v(" "),
+                  _c("v-text-field", {
+                    attrs: {
+                      tabindex: "2",
+                      label: "パスワード",
+                      "prepend-icon": "mdi-lock",
+                      type: _vm.showPassword ? "text" : "password",
+                      "append-icon": _vm.shouPassword
+                        ? "mdi-eye"
+                        : "mdi-eye-off"
+                    },
+                    on: {
+                      "click:append": function($event) {
+                        _vm.showPassword = !_vm.showPassword
+                      },
+                      keyup: function($event) {
+                        if (
+                          !$event.type.indexOf("key") &&
+                          _vm._k(
+                            $event.keyCode,
+                            "enter",
+                            13,
+                            $event.key,
+                            "Enter"
+                          )
+                        ) {
+                          return null
+                        }
+                        return _vm.login.apply(null, arguments)
+                      }
+                    },
+                    model: {
+                      value: _vm.loginForm.password,
+                      callback: function($$v) {
+                        _vm.$set(_vm.loginForm, "password", $$v)
+                      },
+                      expression: "loginForm.password"
+                    }
+                  })
+                ],
+                1
+              )
+            ],
+            1
+          ),
+          _vm._v(" "),
+          _c(
+            "v-card-actions",
+            [
+              _c("v-spacer"),
               _vm._v(" "),
               _c(
-                "v-card-text",
+                "v-tooltip",
+                { attrs: { button: "" } },
                 [
+                  [
+                    _c("v-btn", { attrs: { text: "", color: "success" } }, [
+                      _vm._v("テストログイン")
+                    ])
+                  ],
+                  _vm._v(" "),
                   _c(
-                    "v-form",
+                    "span",
                     [
-                      _c("v-text-field", {
-                        attrs: {
-                          tabindex: "1",
-                          label: "メールアドレス",
-                          "prepend-icon": "mdi-email"
-                        }
-                      }),
-                      _vm._v(" "),
-                      _c("v-text-field", {
-                        attrs: {
-                          tabindex: "2",
-                          label: "パスワード",
-                          "prepend-icon": "mdi-lock"
-                        }
-                      })
+                      _c("v-icon", { attrs: { dark: "", left: "" } }, [
+                        _vm._v("mdi-alert-circle")
+                      ]),
+                      _vm._v("一定期間が立つとアカウントは初期化されます。 ")
                     ],
                     1
                   )
                 ],
-                1
+                2
               ),
               _vm._v(" "),
-              _c(
-                "v-card-actions",
-                [
-                  _c("v-spacer"),
-                  _vm._v(" "),
-                  _c(
-                    "v-tooltip",
-                    { attrs: { button: "" } },
-                    [
-                      [
-                        _c("v-btn", { attrs: { text: "", color: "success" } }, [
-                          _vm._v("テストログイン")
-                        ])
-                      ],
-                      _vm._v(" "),
-                      _c(
-                        "span",
-                        [
-                          _c("v-icon", { attrs: { dark: "", left: "" } }, [
-                            _vm._v("mdi-alert-circle")
-                          ]),
-                          _vm._v(
-                            "一定期間が立つとアカウントは初期化されます。 "
-                          )
-                        ],
-                        1
-                      )
-                    ],
-                    2
-                  ),
-                  _vm._v(" "),
-                  _c("v-btn", { attrs: { color: "info" } }, [
-                    _vm._v("ログイン")
-                  ])
-                ],
-                1
-              )
+              _c("v-btn", { attrs: { color: "info" } }, [_vm._v("ログイン")])
             ],
             1
           )
@@ -41701,6 +41752,16 @@ if (inBrowser && window.Vue) {
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (VueRouter);
 
+
+/***/ }),
+
+/***/ "./node_modules/vue-the-mask/dist/vue-the-mask.js":
+/*!********************************************************!*\
+  !*** ./node_modules/vue-the-mask/dist/vue-the-mask.js ***!
+  \********************************************************/
+/***/ (function(module) {
+
+(function(e,t){ true?module.exports=t():0})(this,function(){return function(e){function t(r){if(n[r])return n[r].exports;var a=n[r]={i:r,l:!1,exports:{}};return e[r].call(a.exports,a,a.exports,t),a.l=!0,a.exports}var n={};return t.m=e,t.c=n,t.i=function(e){return e},t.d=function(e,n,r){t.o(e,n)||Object.defineProperty(e,n,{configurable:!1,enumerable:!0,get:r})},t.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(n,"a",n),n},t.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},t.p=".",t(t.s=10)}([function(e,t){e.exports={"#":{pattern:/\d/},X:{pattern:/[0-9a-zA-Z]/},S:{pattern:/[a-zA-Z]/},A:{pattern:/[a-zA-Z]/,transform:function(e){return e.toLocaleUpperCase()}},a:{pattern:/[a-zA-Z]/,transform:function(e){return e.toLocaleLowerCase()}},"!":{escape:!0}}},function(e,t,n){"use strict";function r(e){var t=document.createEvent("Event");return t.initEvent(e,!0,!0),t}var a=n(2),o=n(0),i=n.n(o);t.a=function(e,t){var o=t.value;if((Array.isArray(o)||"string"==typeof o)&&(o={mask:o,tokens:i.a}),"INPUT"!==e.tagName.toLocaleUpperCase()){var u=e.getElementsByTagName("input");if(1!==u.length)throw new Error("v-mask directive requires 1 input, found "+u.length);e=u[0]}e.oninput=function(t){if(t.isTrusted){var i=e.selectionEnd,u=e.value[i-1];for(e.value=n.i(a.a)(e.value,o.mask,!0,o.tokens);i<e.value.length&&e.value.charAt(i-1)!==u;)i++;e===document.activeElement&&(e.setSelectionRange(i,i),setTimeout(function(){e.setSelectionRange(i,i)},0)),e.dispatchEvent(r("input"))}};var s=n.i(a.a)(e.value,o.mask,!0,o.tokens);s!==e.value&&(e.value=s,e.dispatchEvent(r("input")))}},function(e,t,n){"use strict";var r=n(6),a=n(5);t.a=function(e,t){var o=!(arguments.length>2&&void 0!==arguments[2])||arguments[2],i=arguments[3];return Array.isArray(t)?n.i(a.a)(r.a,t,i)(e,t,o,i):n.i(r.a)(e,t,o,i)}},function(e,t,n){"use strict";function r(e){e.component(s.a.name,s.a),e.directive("mask",i.a)}Object.defineProperty(t,"__esModule",{value:!0});var a=n(0),o=n.n(a),i=n(1),u=n(7),s=n.n(u);n.d(t,"TheMask",function(){return s.a}),n.d(t,"mask",function(){return i.a}),n.d(t,"tokens",function(){return o.a}),n.d(t,"version",function(){return c});var c="0.11.1";t.default=r,"undefined"!=typeof window&&window.Vue&&window.Vue.use(r)},function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r=n(1),a=n(0),o=n.n(a),i=n(2);t.default={name:"TheMask",props:{value:[String,Number],mask:{type:[String,Array],required:!0},masked:{type:Boolean,default:!1},tokens:{type:Object,default:function(){return o.a}}},directives:{mask:r.a},data:function(){return{lastValue:null,display:this.value}},watch:{value:function(e){e!==this.lastValue&&(this.display=e)},masked:function(){this.refresh(this.display)}},computed:{config:function(){return{mask:this.mask,tokens:this.tokens,masked:this.masked}}},methods:{onInput:function(e){e.isTrusted||this.refresh(e.target.value)},refresh:function(e){this.display=e;var e=n.i(i.a)(e,this.mask,this.masked,this.tokens);e!==this.lastValue&&(this.lastValue=e,this.$emit("input",e))}}}},function(e,t,n){"use strict";function r(e,t,n){return t=t.sort(function(e,t){return e.length-t.length}),function(r,a){for(var o=!(arguments.length>2&&void 0!==arguments[2])||arguments[2],i=0;i<t.length;){var u=t[i];i++;var s=t[i];if(!(s&&e(r,s,!0,n).length>u.length))return e(r,u,o,n)}return""}}t.a=r},function(e,t,n){"use strict";function r(e,t){var n=!(arguments.length>2&&void 0!==arguments[2])||arguments[2],r=arguments[3];e=e||"",t=t||"";for(var a=0,o=0,i="";a<t.length&&o<e.length;){var u=t[a],s=r[u],c=e[o];s&&!s.escape?(s.pattern.test(c)&&(i+=s.transform?s.transform(c):c,a++),o++):(s&&s.escape&&(a++,u=t[a]),n&&(i+=u),c===u&&o++,a++)}for(var f="";a<t.length&&n;){var u=t[a];if(r[u]){f="";break}f+=u,a++}return i+f}t.a=r},function(e,t,n){var r=n(8)(n(4),n(9),null,null);e.exports=r.exports},function(e,t){e.exports=function(e,t,n,r){var a,o=e=e||{},i=typeof e.default;"object"!==i&&"function"!==i||(a=e,o=e.default);var u="function"==typeof o?o.options:o;if(t&&(u.render=t.render,u.staticRenderFns=t.staticRenderFns),n&&(u._scopeId=n),r){var s=u.computed||(u.computed={});Object.keys(r).forEach(function(e){var t=r[e];s[e]=function(){return t}})}return{esModule:a,exports:o,options:u}}},function(e,t){e.exports={render:function(){var e=this,t=e.$createElement;return(e._self._c||t)("input",{directives:[{name:"mask",rawName:"v-mask",value:e.config,expression:"config"}],attrs:{type:"text"},domProps:{value:e.display},on:{input:e.onInput}})},staticRenderFns:[]}},function(e,t,n){e.exports=n(3)}])});
 
 /***/ }),
 
--- backend/resources/js/views/Login.vue
+++ backend/resources/js/views/Login.vue
@@ -1,40 +1,64 @@
 <template>
   <v-row>
-    <v-col cols="12" md="5">
-      <v-card>
-        <v-card-title>
-          <h1>ログイン画面だよ</h1>
-        </v-card-title>
-
-        <v-card-title>
-          <h2 class="title">ログイン</h2>
-        </v-card-title>
-
-        <v-card-text>
-          <v-form>
-            <v-text-field tabindex="1" label="メールアドレス" prepend-icon="mdi-email" />
-            <v-text-field tabindex="2" label="パスワード" prepend-icon="mdi-lock" />
-          </v-form>
-        </v-card-text>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-tooltip button>
-            <template>
-              <v-btn text color="success">テストログイン</v-btn>
-            </template>
-            <span> <v-icon dark left>mdi-alert-circle</v-icon>一定期間が立つとアカウントは初期化されます。 </span>
-          </v-tooltip>
-
-          <v-btn color="info">ログイン</v-btn>
-        </v-card-actions>
-      </v-card>
+    <v-spacer></v-spacer>
+    <v-col cols="12" md="5" class="mt-n11">
+      <!-- <v-img>TODO: 画像を追加</v-img> -->
     </v-col>
+
+    <v-spacer></v-spacer>
+    <v-card>
+      <v-card-title>
+        <h1>ログイン画面だよ</h1>
+      </v-card-title>
+
+      <v-card-title class="pb-0">
+        <h2 class="title">ログイン</h2>
+      </v-card-title>
+
+      <v-card-text>
+        <v-form>
+          <v-text-field tabindex="1" v-model="loginForm.email" label="メールアドレス" prepend-icon="mdi-email" />
+          <v-text-field
+            tabindex="2"
+            v-model="loginForm.password"
+            label="パスワード"
+            prepend-icon="mdi-lock"
+            :type="showPassword ? 'text' : 'password'"
+            :append-icon="shouPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = !showPassword"
+            @keyup.enter="login"
+          />
+        </v-form>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-tooltip button>
+          <template>
+            <v-btn text color="success">テストログイン</v-btn>
+          </template>
+          <span> <v-icon dark left>mdi-alert-circle</v-icon>一定期間が立つとアカウントは初期化されます。 </span>
+        </v-tooltip>
+
+        <v-btn color="info">ログイン</v-btn>
+      </v-card-actions>
+    </v-card>
   </v-row>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      snackbar: false,
+      showPassword: false,
+      loginForm: {
+        email: '',
+        password: ''
+      }
+    }
+  }
+}
 </script>
 
 <style></style>
