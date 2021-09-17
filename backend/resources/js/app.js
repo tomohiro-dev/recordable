0ee0177f2f20beff11a355f243ccdca531ed9e69
soregashi-27 <tomo.soul.15@gmail.com>
2021-09-17 19:56:45 +0900

    Vuexを使う準備

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -10,6 +10,8 @@ import Vue from 'vue'
 import router from './router'
 import App from './App.vue'
 
+import store from './store'
+
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import '@mdi/font/css/materialdesignicons.css'
