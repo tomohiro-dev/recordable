a25591324e9b3cc4f239d3df924bd0d98637f4f7
soregashi-27 <tomo.soul.15@gmail.com>
2021-09-20 11:32:33 +0900

    Navbarのレイアウトを調整

--- backend/resources/js/components/NavBar.vue
+++ backend/resources/js/components/NavBar.vue
@@ -6,8 +6,8 @@
       <!-- todo: ロゴを追加する時に削除する -->
     </v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-btn rounded to="/login" text>ログイン</v-btn>
-    <v-btn rounded to="/register" text>新規登録</v-btn>
+    <v-btn to="/login" text>ログイン</v-btn>
+    <v-btn to="/register" text>新規登録</v-btn>
   </v-app-bar>
 </template>
 
