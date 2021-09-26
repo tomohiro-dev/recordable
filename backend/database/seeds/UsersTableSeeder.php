d69598f25eb777dd09f563aef2f2a56d594d52d3
soregashi-27 <tomo.soul.15@gmail.com>
2021-09-26 19:49:07 +0900

    Userでのseeder実行成功

---
+++ backend/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        #初期化
+        DB::table('users')->delete();
+
+        #テストデータ挿入
+        DB::table('users')->insert([
+            'name'    => 'user1',
+            'email' => 'user1@example.com',
+            'password' => Hash::make('secret'),
+            'remember_token' => str_random(10),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+    }
+}
