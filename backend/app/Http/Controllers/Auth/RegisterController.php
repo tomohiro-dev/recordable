d66902e1fdde19854029d4cfb1a78af2c2fb280b
soregashi-27 <tomo.soul.15@gmail.com>
2021-10-02 11:27:07 +0900

    予期せぬエラーを追加

--- backend/app/Http/Controllers/Auth/RegisterController.php
+++ backend/app/Http/Controllers/Auth/RegisterController.php
@@ -76,5 +76,6 @@ class RegisterController extends Controller
     protected function registered(Request $request, $user)
     {
         return $user;
+        throw new \Exception('予期せぬエラー');
     }
 }
