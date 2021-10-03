8f8884efaf1171937054cbcc66dd745483192904
soregashi-27 <tomo.soul.15@gmail.com>
2021-10-03 15:06:53 +0900

    「データがありません」の表示

--- backend/resources/js/views/Dashboard.vue
+++ backend/resources/js/views/Dashboard.vue
@@ -64,7 +64,7 @@
           <v-tab>今月（時間）</v-tab>
         </v-tabs>
 
-        <v-tab v-model="pie">
+        <v-tabs-items v-model="pie">
           <v-tab-item>
             <v-card>
               <div class="py-4">
@@ -76,14 +76,27 @@
                   >
                   </vue-loading>
                 </div>
+              </div>
 
-                <div>
-                  <p>データない</p>
+              <div>
+                <div class="py-4">
+                  <chart
+                   :options="chartPie"
+                   autoresize
+                  >
+                  </chart>
+                  <v-row>
+                    <v-col>
+                      <!-- <v-img>TODO: 画像追加</v-img> -->
+                      <p class="mt-2 subtitle-1 text-center">データがありません。</p>
+                    </v-col>
+                  </v-row>
                 </div>
               </div>
+
             </v-card>
           </v-tab-item>
-        </v-tab>
+        </v-tabs-items>
       </v-col>
 
       <v-col cols="12" md="7">
@@ -94,12 +107,42 @@
         <v-tabs v-model="stack">
           <v-tab>今週（分）</v-tab>
         </v-tabs>
+
+        <v-tabs-items v-model="stack">
+          <v-tab-item>
+            <v-card>
+              <div class="py-4" v-if="loading.stack">
+                <div class="echarts d-flex justify-center align-center">
+                  <vue-loading
+                   type="bars"
+                   color="#696969"
+                   :size="{ width: '50px', height: '50px' }"
+                  >
+                  </vue-loading>
+                </div>
+              </div>
+
+              <div class="py-4" else>
+                <chart
+                 :options="chartStackWeek"
+                 autoresize
+                >
+                </chart>
+                <div>
+                  <v-row>
+                    <v-col>
+                      <!-- <v-img>TODO: 画像追加</v-img> -->
+                      <p class="mt-2 subtitle-1 text-center">データがありません。</p>
+                    </v-col>
+                  </v-row>
+                </div>
+              </div>
+            </v-card>
+          </v-tab-item>
+        </v-tabs-items>
       </v-col>
 
-
-
     </v-row>
-
   </div>
 </template>
 
@@ -117,7 +160,14 @@ export default {
         thisWeek: 0,
         thisMonth: 0,
         todal: 0
-      }
+      },
+      loading: {
+        pie: true,
+        stack: true
+      },
+      stack: "",
+      pie: "",
+      errorMessage: ""
     }
   },
 }
