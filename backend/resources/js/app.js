cd90c25bb86dea5e0d6f749a4bb07959539c3f29
soregashi-27 <tomo.soul.15@gmail.com>
2021-10-03 17:41:01 +0900

    dependenciesの追加(vue,countup.js,DatetimePicker)

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -20,12 +20,17 @@ import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import '@mdi/font/css/materialdesignicons.css'
 import VueTheMask from 'vue-the-mask'
+import DatetimePicker from 'vuetify-datetime-picker'
+import VueLoading from 'vue-loading-template'
+import './echarts'
 
 // window.Vue = require('vue')
 window.Vue = Vue
 Vue.use(Vuetify)
 Vue.use(VueTheMask)
 Vue.use(VueRouter)
+Vue.use(DatetimePicker)
+Vue.use(VueLoading)
 
 const createApp = async () => {
   await store.dispatch('auth/currentUser')
