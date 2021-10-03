2bad93649b35ef575841c17f979f6b986eaa3f3a
soregashi-27 <tomo.soul.15@gmail.com>
2021-10-03 13:35:44 +0900

    レコードの数字を追加

--- backend/resources/js/views/Dashboard.vue
+++ backend/resources/js/views/Dashboard.vue
@@ -1,11 +1,69 @@
 <template>
   <div>
-    <h1>Dashboard画面やで。</h1>
+    <!-- TODO: snackbarの追加 -->
+
+    <span class="title">
+      <v-icon class="mr-2 mb-1">mdi-archive-clock</v-icon>レコード
+    </span>
+    <v-row>
+      <v-col cols="12" sm="4" md="3">
+        <v-card>
+          <v-card-title class="pb-2">今日</v-card-title>
+          <v-divider class="my-0 mx-4"></v-divider>
+          <v-card-text class="display-2">
+            <ICountUp :sndVal="record.today" />
+            <span class="headline">分</span>
+          </v-card-text>
+        </v-card>
+      </v-col>
+
+      <v-col cols="12" sm="4" md="3">
+        <v-card>
+          <v-card-title class="pb-2">今週({{ thisWeek }})</v-card-title>
+          <v-divider class="my-0 mx-4"></v-divider>
+          <v-card-text class="display-2">
+            <ICountUp
+             :endVal="record.thisweek"
+             :options="{ decimals: '.', decimalPlaces: 1}"
+            />
+            <span class="headline">時間</span>
+          </v-card-text>
+        </v-card>
+      </v-col>
+
+      <v-col cols="12" sm="4" md="3">
+        <v-card>
+          <v-card-title class="pb-2">今日({{ thisMonth }})</v-card-title>
+          <v-divider class="my-0 mx-4"></v-divider>
+          <v-card-text class="display-2">
+            <ICountUp :sndVal="record.thisMonth" />
+            <span class="headline">時間</span>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
   </div>
 </template>
 
 <script>
-export default {}
+import ICountUp from "vue-countup-v2"
+
+export default {
+  components: {
+    ICountUp
+  },
+  data() {
+    return {
+      record: {
+        today: 0,
+        thisWeek: 0,
+        thisMonth: 0,
+        todal: 0
+      }
+    }
+  },
+}
 </script>
 
 <style scoped>
