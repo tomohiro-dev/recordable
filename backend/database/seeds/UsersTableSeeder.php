576ed9366490cd7d116d41a97c5a8a8a967cdb70
soregashi-27 <tomo.soul.15@gmail.com>
2021-10-06 08:52:13 +0900

    seederのデータに'seeder'と追加

--- backend/database/seeds/UsersTableSeeder.php
+++ backend/database/seeds/UsersTableSeeder.php
@@ -16,9 +16,9 @@ class UsersTableSeeder extends Seeder
 
         #テストデータ挿入
         DB::table('users')->insert([
-            'name'    => 'user1',
-            'email' => 'user1@example.com',
-            'password' => 'userexample',
+            'name'    => 'user1 seeder',
+            'email' => 'user1seeder@example.com',
+            'password' => 'user1seeder',
             'remember_token' => str_random(10),
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s')
