a84eff7519261c3fc5151a2c4e80c80ae7e8397c
soregashi-27 <tomo.soul.15@gmail.com>
2021-10-06 23:15:52 +0900

    sourceMapsをコメントアウト

--- backend/webpack.mix.js
+++ backend/webpack.mix.js
@@ -20,7 +20,8 @@ mix
   .vue()
   .sass('resources/sass/app.scss', 'public/css')
 
-mix.sourceMaps().js('node_modules/popper.js/dist/popper.js', 'public/js').sourceMaps()
+// mix.sourceMaps().js('node_modules/popper.js/dist/popper.js', 'public/js').sourceMaps()
+// TODO: source-map-explorerをinstallする
 
 // Ready to Deploy
 //  mix.browserSync('OriginalDomain or IPアドレスを記述')
