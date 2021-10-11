c7ada4fa24cde710adce61d4ae637b191c6ac044
soregashi-27 <tomo.soul.15@gmail.com>
2021-10-11 14:45:56 +0900

    システムエラーのsnackbarを追加

--- backend/resources/js/App.vue
+++ backend/resources/js/App.vue
@@ -1,6 +1,9 @@
 <template>
   <v-app>
-    <!-- TODO: snackbar追加 -->
+    <v-snackbar top v-model="systemErrorSnackbar" color="error">
+      A system error has occurred.
+      <v-btn text @click="systemErrorSnackbar = false">close</v-btn>
+    </v-snackbar>
     <NavBar />
     <NavDrawer />
     <v-main>
@@ -15,12 +18,17 @@
 import NavBar from './components/NavBar.vue'
 import NavDrawer from './components/NavDrawer.vue'
 
-// import { INTERNAL_SERVER_ERROR } from './util'
+import { INTERNAL_SERVER_ERROR } from './util'
 
 export default {
   components: {
     NavBar,
     NavDrawer
+  },
+  data() {
+    return {
+      systemErrorSnackbar: false
+    }
   },
   computed: {
     // error storeのcode state 取得
@@ -29,16 +37,15 @@ export default {
     }
   },
   watch: {
-    //TODO: snackbar追加時にコメントアウト解除
-    // errorCode: {
-    //   handler(val) {
-    //     if (val === INTERNAL_SERVER_ERROR) {
-    //       this.systemErrorSnackbar = true
-    //     }
-    //   },
-    //   //createdで呼び出し
-    //   immediate: true
-    // },
+    errorCode: {
+      handler(val) {
+        if (val === INTERNAL_SERVER_ERROR) {
+          this.systemErrorSnackbar = true
+        }
+      },
+      //createdで呼び出し
+      immediate: true
+    },
     //同じrouteの異なるパラメータで画面遷移しても、vue-routerは画面を再描画しないのでwatchで監視する
     $route() {
       this.$store.commit('error/SET_CODE', null)
