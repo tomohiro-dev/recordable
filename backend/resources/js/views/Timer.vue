32f01cfee74a58a410677d76f5d1b3691d609156
soregashi-27 <tomo.soul.15@gmail.com>
2021-10-17 08:37:03 +0900

    紙吹雪のデザイン修正

--- backend/resources/js/views/Timer.vue
+++ backend/resources/js/views/Timer.vue
@@ -955,11 +955,29 @@ export default {
           this.activeTimerString = "Calculating..."
           this.snackbar.activeTimer = false;
           this.snackbar.done = true
-          confetti({
-            particleCount: 100,
-            spread: 70,
-            origin: { y: 0.6 }
-          })
+          // confetti({
+          var end = Date.now() + (3 * 1000);
+          // go Buckeyes!
+          var colors = ['#bb0000', '#ffffff'];
+          (function frame() {
+            confetti({
+              particleCount: 2,
+              angle: 60,
+              spread: 55,
+              origin: { x: 0 },
+              colors: colors
+            });
+            confetti({
+              particleCount: 2,
+              angle: 120,
+              spread: 55,
+              origin: { x: 1 },
+              colors: colors
+            });
+            if (Date.now() < end) {
+              requestAnimationFrame(frame);
+            }
+          }());
           this.$store.dispatch("timer/fetchActive", false)
         })
         .catch(err => {
