0d94a8d04af4190842c4d729181a59e5bf9e99ce
soregashi-27 <tomo.soul.15@gmail.com>
2021-11-14 19:47:41 +0900

    DashboardErrorをcomponentで切り出し

---
+++ backend/resources/js/components/snackbar/DashboardError.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-snackbar top v-model="snackbar.error" color="error">
+      An error has occurred. message：{{ errorMessage }}
+      <v-btn text @click="snackbar.error = false">CLOSE</v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      snackbar: {
+        error: false
+      },
+      errorMessage: ""
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
